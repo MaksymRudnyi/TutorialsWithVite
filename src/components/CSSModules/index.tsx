cf7650d4309e8012483fb38e130a749776a88148
Maksym Rudnyi <mrudnyi@idiq.com>
2023-04-07 14:00:06 +0300

    css modules

--- src/components/CSSModules/index.tsx
+++ src/components/CSSModules/index.tsx
@@ -1,6 +1,33 @@
 import React from 'react';
 import styles from './styles.module.css';
 
-const MovieCard = ({ movie, rating }) => {}
+function MoviePoster({ movie, rating }) {
+  return (
+    <div className={styles.movie_card} id="bright">
+      <div className={styles.info_section}>
+        <div className={styles.movie_header}>
+          <img className={styles.locandina}
+               src={movie.imageUrl}/>
+          <h1>{movie.title}</h1>
+          <h4>{movie.year}, {movie.director}</h4>
+          <span className={styles.minutes}>{movie.lengthMinutes} min</span>
+          <span className={`${styles.rating} ${styles[rating.toLowerCase()]}`}>{rating}</span>
+          <p className={styles.type}>{movie.genres?.map((g) => `${g},`)}</p>
+        </div>
+        <div className={styles.movie_desc}>
+          <p className={styles.text}>{movie.description}</p>
+        </div>
+        <div className={styles.movie_social}>
+          <ul>
+            <li><i className="material-icons">share</i></li>
+            <li><i className="material-icons"></i></li>
+            <li><i className="material-icons">chat_bubble</i></li>
+          </ul>
+        </div>
+      </div>
+      <div className={styles.blur_back} style={{backgroundImage: `url("${movie.imageBackground}")`}}></div>
+    </div>
+  );
+}
 
-export default MovieCard;
+export default MoviePoster;
